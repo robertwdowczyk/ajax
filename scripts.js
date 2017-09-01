fc1e37760fa2f436394a5e4ef384c27c40557cd7
robertwdowczyk <30527317+robertwdowczyk@users.noreply.github.com>
2017-09-01 13:27:32 +0200

    Random_quote

--- scripts.js
+++ scripts.js
@@ -1,20 +1,37 @@
-﻿var url = 'http://api.icndb.com/jokes/random';
+﻿var prefix = "https://cors-anywhere.herokuapp.com/";
+var tweetLink = "https://twitter.com/intent/tweet?text="; 
+var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
-var button = document.getElementById('get-joke'); 
-button.addEventListener('click', function(){ 
-    getJoke(); 
+function getQuote() { 
+    $.getJSON(prefix + quoteUrl, createTweet);
+    $.ajaxSetup({ cache: false }) 
+}
+
+function createTweet(input) { 
+    var data = input[0]; 
+
+    var quoteText = $(data.content).text().trim(); 
+    var quoteAuthor = data.title; 
+
+    if (!quoteAuthor.length) { 
+        quoteAuthor = "Unknown author";
+    }
+
+    var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+
+    if (tweetText.length > 140) { 
+        getQuote(); 
+    }  else { 
+        var tweet = tweetLink + encodeURIComponent(tweetText); 
+        $('.quote').text(quoteText); 
+        $('.author').text("Author: " + quoteAuthor); 
+        $('.tweet').attr('href', tweet); 
+    }
+}
+
+$(document).ready(function() { 
+    getQuote(); 
+    $('.trigger').click(function() { 
+        getQuote(); 
+    }); 
 });
-var paragraph = document.getElementById('joke');
-function getJoke() { 
-    var xhr = new XMLHttpRequest(); 
-    xhr.open('GET', url); 
-    xhr.addEventListener('load', function(){ 
-        var response = JSON.parse(xhr.response); 
-        paragraph.innerHTML = response.value.joke;
-        }); 
-    xhr.send(); 
-}
-document.body.onload = function() {
-    getJoke();
-}
-    
